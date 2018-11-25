a4e50f2aeb59bdf6be974891ebcaf2dae58450db
rossi <rossi@rossiMacBook-pro.local>
2018-11-26 00:55:08 +0900

    ex01 completed

--- 01_第1回目/ex01/ex01.go
+++ 01_第1回目/ex01/ex01.go
@@ -1,7 +1,22 @@
 package main
 
 import "fmt"
+import "os"
+import "os/exec"
 
 func main() {
-	fmt.Printf("Hello World¥n")
+	fmt.Printf("Command \"ls\" returned :\n%s", comexec("ls"))
+}
+
+func comexec(comstr string)([]byte){
+	cmd := exec.Command(comstr)
+	out, err := cmd.Output()
+	/* Here's another option...And it's much more simple.
+			out, err := exec.Command(comstr).Output()		*/
+	if err != nil{
+		fmt.Printf("Oops -> %s", err)
+		os.Exit(1)
+		/* Godoc says code "0" indicates success. */
+	}
+	return out
 }
