7d8c6c09933277affdd9aad85750d7f2607c6d65
6ssi <hnakahara@r.recruit.co.jp>
2018-11-26 23:25:33 +0900

    counting and printing ARGS part as function

--- 01_第1回目/ex02/ex02.go
+++ 01_第1回目/ex02/ex02.go
@@ -8,12 +8,15 @@ import (
 )
 
 func main() {
-	inputargs := []string(os.Args)
+	countargs(os.Args)
+	printargs(os.Args)
+}
+
+func countargs(inputargs []string) {
 	fmt.Printf("You have "+"%d"+" arguments.\n", len(inputargs[1:]))
-	/* inputargs[1:]
-	is same as
-		inputargs[1 : len(inputargs)]
-	*/
+}
+
+func printargs(inputargs []string) {
 	fmt.Printf("Here are your arguments:\n")
 	for i := 1; i < len(inputargs); i++ {
 		fmt.Printf("%s\n", inputargs[i])
