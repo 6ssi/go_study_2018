a5cb390a96655969fd2fcaa4b0baa0e70acfe297
6ssi <hnakahara@r.recruit.co.jp>
2018-11-26 22:02:41 +0900

    fixed comment and builded a binary for ex01

--- 01_第1回目/ex01/ex01.go
+++ 01_第1回目/ex01/ex01.go
@@ -1,5 +1,6 @@
-/* ex01 */
 package main
+
+/* ex01 */
 
 import "fmt"
 import "os"
@@ -9,12 +10,12 @@ func main() {
 	fmt.Printf("Command \"ls\" returned :\n%s", comexec("ls"))
 }
 
-func comexec(comstr string)([]byte){
+func comexec(comstr string) []byte {
 	cmd := exec.Command(comstr)
 	out, err := cmd.Output()
 	/* Here's another option...And it's much more simple.
-			out, err := exec.Command(comstr).Output()		*/
-	if err != nil{
+	out, err := exec.Command(comstr).Output()		*/
+	if err != nil {
 		fmt.Printf("Oops -> %s", err)
 		os.Exit(1)
 		/* Godoc says code "0" indicates success. */
