efed627c43171e2091976fd2b90283ca8d35ed1f
6ssi <hnakahara@r.recruit.co.jp>
2018-11-27 19:12:29 +0900

    end

--- 01_第1回目/ex04/ex04.go
+++ 01_第1回目/ex04/ex04.go
@@ -3,19 +3,19 @@ package main
 /* ex03 */
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
 func main() {
-	//inputcmd := flag.String("cmd", "ls", "Type your command")
-	//inputopt := flag.String("opt", "l", "Type your option")
-	//flag.Parse()
-	//execcmd := *inputcmd + " -" + *inputopt
-	//fmt.Printf("%s\n", execcmd)
-	comstr := [...]string{"ls", " -l"}
-	cmd := exec.Command(comstr)
+	inputcmd := flag.String("cmd", "ls", "Type your command")
+	inputopt := flag.String("opt", "l", "Type your option")
+	flag.Parse()
+	strcmd := *inputcmd
+	stropt := *inputopt
+	cmd := exec.Command(strcmd, stropt)
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Oops -> %s", err)
