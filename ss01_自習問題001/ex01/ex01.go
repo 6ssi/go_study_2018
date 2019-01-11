250d7633083e742b47a862ca000fd2710e7ee908
6ssi <hnakahara@r.recruit.co.jp>
2019-01-11 21:03:04 +0900

    ex01 completed

--- ss01_自習問題001/ex01/ex01.go
+++ ss01_自習問題001/ex01/ex01.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	slice := [4]int[2,4,10,1]
+	slice := []int{2, 4, 10, 1}
 	sum := 0
-	for i:=0;i<len(slice);i++ {
-		sum += slice[i]
+	for _, g := range slice {
+		sum += g
 	}
-	fmt.Println("%d\n", sum)
+	fmt.Printf("sum : %d\n", sum)
 }
